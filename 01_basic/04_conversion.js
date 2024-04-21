b123224f459ff33f881c026e5ad69fb17a31e777
@nipunmagotra <76024979+NipunMagotra@users.noreply.github.com>
2024-04-22 00:46:44 +0530

    Add files via upload

--- 01_basic/04_conversion.js
+++ 01_basic/04_conversion.js
@@ -35,3 +35,46 @@ console.log( typeof newValue);
 //  false -> "false"
 //  null -> "null"
 //  undefined -> "undefined"
+
+
+//------------------------Operations------------------------------------------------------
+
+let values = 10
+let negValue = -values
+console.log(negValue);
+
+//Some operations
+
+console.log(2+2); // addition  
+console.log(2-2);// subtraction 
+
+console.log(2*2); // multipication
+console.log(2/2); // divide
+
+console.log(2**2); // power
+console.log(2%2); // remainder
+
+let str1 = "hello "
+let str2 = "Nipun"
+let newStr =str1 +str2
+console.log(newStr)
+// expected output hello Nipun --- string concatenation
+
+console.log(1 + "2")     //12
+console.log("1" + 2)     //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32     
+
+
+let counter = 1
+counter++;
+console.log(counter); // increment ->2
+ 
+let counter2 = 1
+counter2--;
+console.log(counter2); // decrement ->0
+
+
+  
+
+
