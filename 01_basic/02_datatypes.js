35bcb4795636b75f481f1e676e4327a7bd012512
@nipunmagotra <76024979+NipunMagotra@users.noreply.github.com>
2024-04-21 23:12:42 +0530

    Add files via upload

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // it helps to treat all the JS code as the newer version.
+
+alert(3+6); // it will show the output in the alert box , but in browser not in the console.
+
+// few examples of the data types
+let name="nipun"
+let age =20
+let isSignedIn= true
+
+//examples of data types javascript = number, string, boolean, undefined, null-> standalone value, 
+//symbol->for uniqueness, object
---
+++ 01_basic/04_conversion.js
@@ -0,0 +1,37 @@
+// num
+let score = 11
+let newScore = Number(score)
+console.log(newScore)
+console.log( typeof newScore)
+
+ // number -> number 
+ // "11abc" -> NaN
+ // true -> 1
+ // false -> 0
+
+
+ //boolean
+ let isLoggedIn = 1
+ let isBoolean = Boolean(isLoggedIn)
+ console.log(isBoolean);  
+
+// 1 -> true
+// 0 -> false
+// boolean -> boolean
+// "" -> false
+// "abc" -> true
+
+
+//String
+
+let value = "great"
+let newValue = String(value)
+console.log(newValue);
+console.log( typeof newValue);
+
+//string -> string
+// 123 -> "123"
+//  true -> "true"
+//  false -> "false"
+//  null -> "null"
+//  undefined -> "undefined"
