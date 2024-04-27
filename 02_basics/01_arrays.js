7584ee351075b7681572d36d1e2d9e74b5b449b8
@nipunmagotra <76024979+NipunMagotra@users.noreply.github.com>
2024-04-27 23:38:51 +0530

    Add files via upload

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,20 +1,20 @@
 //arrays
 
-// const myArray =[0,1,2,3,4,5,6,7,8,9,0]
-// console.log(myArray[0]);
-// const Heroes = ["flash", "battle star galatica","red ranger"  ]
-// console.log(Heroes[2]);
-// const newWayArray = new Array(1,3,4,  "happy ", "singh")
-// console.log(newWayArray[4]);
+const firstArray =[0,1,2,3,4,5,6,7,8,9,0]
+console.log(firstArray[0]);
+const Heroes = ["flash", "battle star galatica","red ranger"  ]
+console.log(Heroes[2]);
+const newWayArray = new Array(1,3,4,  "happy ", "singh")
+console.log(newWayArray[4]);
 
-// const newArray = [1,34,56,64,5,6,4,4]
-// newArray.push("hello") // append hello
-// newArray.pop() // remove last element
-// newArray.unshift("welcome") // add element at the start
-// newArray.shift() // remove element at the start
+const newArray = [1,34,56,64,5,6,4,4]
+newArray.push("hello") // append hello
+newArray.pop() // remove last element
+newArray.unshift("welcome") // add element at the start
+newArray.shift() // remove element at the start
 
-// console.log(newArray.includes(1));
-// console.log(newArray.indexOf(56));
+console.log(newArray.includes(1));
+console.log(newArray.indexOf(56));
 
 
 //  Slice and splice
