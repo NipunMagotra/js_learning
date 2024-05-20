a44c2a0edec2a0373afa23618883844b90741e9f
@nipunmagotra <76024979+NipunMagotra@users.noreply.github.com>
2024-05-20 23:36:53 +0530

    Add files via upload

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,8 +1,9 @@
 function names(){ console.log("Nipun");
 console.log("Magotra");}
 
-// names() calling
-// names reference
+names() //calling
+names //reference
+
  function addTwoNumbers(number1 , number2){
     return number1+number2
  }
@@ -16,3 +17,28 @@ console.log("Magotra");}
     return`${username} just login hua`
  }
  console.log(userLoggedIn("Nipun"));  
+
+
+function calculateCart(...num1){
+   return num1
+}
+console.log(calculateCart(212,4,33,4)); 
+
+// handle objects in functions
+const user ={
+   username : "Nipun",
+   age : 99,
+   price : 10
+}
+function objectUsingFunction(anyobject){
+   console.log(`name is ${anyobject.username} and age is ${anyobject.age}`);
+
+}
+objectUsingFunction(user)
+
+// handle array in functions 
+const newArray  = [200, 22,444,55,6,77,89]
+function returnSecondValue(randomarray){
+   return randomarray[1]
+}
+console.log(returnSecondValue(newArray))
